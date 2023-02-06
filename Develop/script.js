44c0b15d1fe42e50fb7acc0e06c28e72d5f422d6
Gavin McNutt <gavinmcnutt1@gmail.com>
2023-02-05 23:57:27 -0800

    added code into develop folder/script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,100 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs();
+var saveButton = $('.saveBtn')
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+saveButton.on('click' , function () {
+  var hour = 0;
+  for (var i = 0; i < saveButton.length; i++) {
+    if (this === saveButton[i]) {
+      hour = i;
+    }
+
+  }
+
+if (hour < 4) {
+  hour+=9;
+}
+else {
+  hour+=9;
+  hour-=12;
+}
+
+
+
+
+var hourElement = '#hour-' + hour;
+var el = $(hourElement).children()[1].value; // Takes the element and stores in in local storage
+
+localStorage.setItem(hour , el);
+const cat = localStorage.getItem(hour);
+
+var rightNow = dayjs().hour();
+console.log(rightNow);
+$(hourElement).children()[1].value = cat;
+// console.log(cat);
+
+
+
+
+
+
+
+})
+$('#hour-9').children()[1].value = localStorage.getItem(9);
+$('#hour-10').children()[1].value = localStorage.getItem(10);
+$('#hour-11').children()[1].value = localStorage.getItem(11);
+$('#hour-12').children()[1].value = localStorage.getItem(12);
+$('#hour-1').children()[1].value = localStorage.getItem(1);
+$('#hour-2').children()[1].value = localStorage.getItem(2);
+$('#hour-3').children()[1].value = localStorage.getItem(3);
+$('#hour-4').children()[1].value = localStorage.getItem(4);
+$('#hour-5').children()[1].value = localStorage.getItem(5);
+
+var value = 9;
+
+var rightNow = dayjs().hour();
+
+
+
+
+
+for(var i = 0; i < 9; i++) {
+
+  if (rightNow > value) {
+
+    var temp = value;       // Functionality to convert 24 hour time to 12 hour time
+    if (value > 12) {       //
+      temp = value - 12;    //
+    }                       //
+    $('#hour-' + temp).addClass('past')
+
+  }
+  else if (rightNow == value ) {
+
+    var temp = value;       // Functionality to convert 24 hour time to 12 hour time
+    if (value > 12) {       //
+      temp = value - 12;    //
+    }                       //
+    $('#hour-' + temp).addClass('present')
+
+  }
+  else {
+
+    var temp = value;
+    if (value > 12) {
+      temp = value - 12;
+    }
+    $('#hour-' + temp).addClass('future')
+
+  }
+  value++;
+}
+
+
+
 });
